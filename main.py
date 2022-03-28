39ee8900893964e4b664033994ac68949de8632a
Kuan-Yin Chen <git@kuanyin.dev>
2022-03-28 15:30:38 +0000

    update

--- main.py
+++ main.py
@@ -6,41 +6,61 @@ from datetime import datetime
 
 console = Console()
 
-console.log(sh.redis_cli("-p", "5001", "-h", "raft1", "raft.cluster", "init"))
-console.log(sh.redis_cli("-p", "5002", "-h", "raft2",
-            "RAFT.CLUSTER", "JOIN", "raft1:5001"))
-console.log(sh.redis_cli("-p", "5003", "-h", "raft3",
-            "RAFT.CLUSTER", "JOIN", "raft1:5001"))
-# console.log(sh.redis_cli("-p", "5001", "-h", "raft1", "-h", "raft1", "RAFT.INFO", raw=True).stdout.decode())
+console.log(sh.docker_compose("exec", "-T", "client", "redis-cli",
+            "-p", "5001", "-h", "raft1", "raft.cluster", "init"))
+console.log(sh.docker_compose("exec",  "-T", "client", "redis-cli",
+            "-p", "5002", "-h", "raft2", "RAFT.CLUSTER", "JOIN", "raft1:5001"))
+console.log(sh.docker_compose("exec", "-T", "client", "redis-cli",
+            "-p", "5003", "-h", "raft3", "RAFT.CLUSTER", "JOIN", "raft1:5001"))
 
-# ycsb = sh.Command("/YCSB/bin/ycsb")
+# console.log(sh.docker_compose("exec",  "-T", "client", "redis-cli",
+#             "-p", "5002", "-h", "raft2", "--raw", "RAFT.INFO"))
+# console.log(sh.docker_compose("exec",  "-T", "client", "redis-cli",
+#             "-p", "5003", "-h", "raft3", "--raw", "RAFT.INFO"))
 
-# console.log(ycsb("load", "redis", "-s", "-P", "/YCSB/workloads/workloada", "-P", "large.dat", "-p", "redis.host=raft1", "-p", "redis.port=5001", _out="/output/outputLoad.txt"))
-# console.log(ycsb("run", "redis", "-s", "-P", "/YCSB/workloads/workloada", "-P", "large.dat", "-p", "redis.host=raft1", "-p", "redis.port=5001", _out="/output/outputRun.txt"))
+# # ycsb = sh.Command("/YCSB/bin/ycsb")
+
+# # console.log(ycsb("load", "redis", "-s", "-P", "/YCSB/workloads/workloada", "-P", "large.dat", "-p", "redis.host=raft1", "-p", "redis.port=5001", _out="/output/outputLoad.txt"))
+# # console.log(ycsb("run", "redis", "-s", "-P", "/YCSB/workloads/workloada", "-P", "large.dat", "-p", "redis.host=raft1", "-p", "redis.port=5001", _out="/output/outputRun.txt"))
 timestamp = datetime.utcnow()
 console.log("start benchmark")
 
-folder = f"/output/{timestamp}"
+folder = f"./output/{timestamp}"
 mkdir(folder)
 
 for c in range(1, 300, 10):
-    bm = sh.redis_benchmark("-p", "5001", "-h", "raft1", "-q", "-t",
-                            "set,get", "-n", "1000000", "-c", c, "-P", "16", _bg=True)
+    bm = sh.docker_compose("exec", "-T", "client", "redis-benchmark", "-p", "5001", "-h", "raft1", "-q", "-t",
+                           "set,get", "-n", "10000000000", "-c", c, "-P", "16", "-d", "1024", "-l", _bg=True)
     sleep(3)
-    latency = sh.redis_cli("-p", "5001", "-h", "raft1",
-                           "--latency-history", _bg=True, _out=f"{folder}/latency-{c}.txt")
+    latency = sh.docker_compose("exec", "-T", "client", "redis-cli", "-p", "5001", "-h", "raft1",
+                                "--latency-history", _bg=True, _out=f"{folder}/latency-{c}.txt")
     f = open(f"{folder}/tp-{c}.txt", "w")
 
     for i in range(10):
-        stats = sh.redis_cli("-p", "5001", "-h", "raft1", "info", "stats")
+        stats = sh.docker_compose(
+            "exec",  "-T", "client", "redis-cli", "-p", "5002", "-h", "raft2", "info", "stats")
+        data = stats.stdout.decode().split("\r\n")
+        console.log("raft2", data[3])
+        stats = sh.docker_compose(
+            "exec",  "-T", "client", "redis-cli", "-p", "5003", "-h", "raft3", "info", "stats")
+        data = stats.stdout.decode().split("\r\n")
+        console.log("raft3", data[3])
+        stats = sh.docker_compose(
+            "exec",  "-T", "client", "redis-cli", "-p", "5001", "-h", "raft1", "info", "stats")
         data = stats.stdout.decode().split("\r\n")
         console.log(data[3])
         f.write(data[3].split(":")[1]+"\n")
         sleep(1)
 
     f.close()
-    latency.kill()
-    bm.kill()
+    try:
+        latency.kill()
+    except Exception:
+        pass
+    try:
+        bm.kill()
+    except Exception:
+        pass
     sleep(3)
 
 console.log("end benchmark")
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@ sudo cgcreate -g memory:raft1
 echo 524288000 > /sys/fs/cgroup/memory/raft1/memory.limit_in_bytes
 
 
-docker update --memory=104857600 --memory-swap=104857601 --cpu-shares=2 redisraft-reliability-evaluation_raft2_1
+docker update --memory=6291456 --memory-swap=6291456 --cpu-shares=2 redisraft-reliability-evaluation_raft1_1
