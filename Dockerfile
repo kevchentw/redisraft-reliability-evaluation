b7475fd728bd90f8783bcecf5c40346d50746b48
Kuan-Yin Chen <git@kuanyin.dev>
2022-03-29 02:19:32 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ARG DEBIAN_FRONTEND=noninteractive
 ENV TZ=America/Chicago
 WORKDIR /tmp
 COPY requirements.txt /tmp
-RUN apt-get update  && apt-get install -y build-essential libssl-dev git cmake default-jdk maven python3-pip python
+RUN apt-get update && apt-get install -y build-essential libssl-dev git cmake default-jdk maven python3-pip python stress
 RUN pip install -r /tmp/requirements.txt
 RUN git clone https://github.com/RedisLabs/redisraft.git
 RUN cd /tmp/redisraft && mkdir build && cd build && cmake .. && make
@@ -12,8 +12,8 @@ RUN git clone https://github.com/redis/redis
 RUN cd redis && make && make install
 RUN mkdir /data/
 RUN mv /tmp/redisraft/redisraft.so /data/redisraft.so
-RUN git clone http://github.com/brianfrankcooper/YCSB.git /YCSB
-WORKDIR /YCSB
-RUN cd /YCSB && mvn -pl site.ycsb:redis-binding -am clean package
-COPY main.py /YCSB
-COPY large.dat /YCSB
+# RUN git clone http://github.com/brianfrankcooper/YCSB.git /YCSB
+# WORKDIR /YCSB
+# RUN cd /YCSB && mvn -pl site.ycsb:redis-binding -am clean package
+# COPY main.py /YCSB
+# COPY large.dat /YCSB
--- main.py
+++ main.py
@@ -12,58 +12,86 @@ console = Console()
 # console.log(sh.docker_compose("exec",  "-T", "client", "redis-cli",
 #             "-p", "5003", "-h", "raft3", "--raw", "RAFT.INFO"))
 
-timestamp = datetime.utcnow()
-console.log("start benchmark")
-
-folder = f"./output/{timestamp}"
-mkdir(folder)
-
 cluster = [
     ("raft1", "5001"),
     ("raft2", "5002"),
     ("raft3", "5003")
 ]
 
-result = []
-sh.docker_compose("up", "--force-recreate", "-d")
 
-for c in range(1, 200, 5):
-    for p in [1]:
-        console.log("start cluser")
-        console.log("join raft")
+def start_cluster():
+    console.log("start cluser")
+    sh.docker_compose("down")
+    sh.docker_compose("--compatibility", "up", "-d")
+    console.log("join raft")
 
-        console.log(sh.docker_compose("exec", "-T", "client", "redis-cli",
-                    "-p", "5001", "-h", "raft1", "raft.cluster", "init"))
-        console.log(sh.docker_compose("exec",  "-T", "client", "redis-cli",
-                    "-p", "5002", "-h", "raft2", "RAFT.CLUSTER", "JOIN", "raft1:5001"))
-        console.log(sh.docker_compose("exec", "-T", "client", "redis-cli",
-                    "-p", "5003", "-h", "raft3", "RAFT.CLUSTER", "JOIN", "raft1:5001"))
+    console.log(sh.docker_compose("exec", "-T", "client", "redis-cli",
+                "-p", "5001", "-h", "raft1", "raft.cluster", "init"))
+    console.log(sh.docker_compose("exec",  "-T", "client", "redis-cli",
+                "-p", "5002", "-h", "raft2", "RAFT.CLUSTER", "JOIN", "raft1:5001"))
+    console.log(sh.docker_compose("exec", "-T", "client", "redis-cli",
+                "-p", "5003", "-h", "raft3", "RAFT.CLUSTER", "JOIN", "raft1:5001"))
 
-        res = {"c" : c, "p": p, "latency" : [], "throughput" : []}
-        bm_process = sh.docker_compose("exec", "-T", "client", "redis-benchmark", "-p", "5001", "-h", "raft1", "-q", "-t",
-                            "set,get", "-n", "10000000000", "-c", str(c), "-P", str(p), "-d", "1024", "-l", _bg=True, _bg_exc=False)
 
-        sleep(5)
-        for (host, port) in cluster:
-            stats_process = sh.docker_compose(
-                "exec",  "-T", "client", "redis-cli", "-p", port, "-h", host, "info", "stats")
-            throughput = stats_process.stdout.decode().split("\r\n")[3].split(":")[1]
-            res["throughput"].append(throughput)
-            latency_process = sh.docker_compose("exec", "-T", "client", "redis-cli", "-p", port, "-h", host,
-                                        "--latency", _bg=True)
-            latency = latency_process.stdout.decode("utf-8").strip().split(" ")[2]
-            res["latency"].append(latency)
+def benchmark(client_cnt, thread_cnt, fn, round=1, bg=False):
+    return sh.docker_compose("exec", "-T", "benchmark", "memtier_benchmark", "-p", "5001", "-s", "raft1",
+                             "-c", str(client_cnt), "-t", str(
+                                 thread_cnt), "--out-file", fn,
+                             "--test-time", 10, "--data-offset", "1048575", "--data-size", "1024", "-x", round, _bg=bg, _bg_exc=False)
 
-        with open(f"{folder}/result.json", "a+") as f:
-            f.write(json.dumps(res)+"\n")
-        sleep(1)
-        print(res)
+
+def run_baseline():
+    timestamp = datetime.utcnow()
+    console.log("start benchmark")
+
+    folder = f"./output/{timestamp}"
+    mkdir(folder)
+    start_cluster()
+    for client_cnt in range(1, 70, 2):
+        console.log(client_cnt)
+        for thread_cnt in [4]:
+            benchmark(client_cnt, thread_cnt,
+                      f"{folder}/baseline-{client_cnt}-{thread_cnt}.txt")
+
+        console.log("end benchmark")
+
+
+def run_fail_injection(target: str, memory: bool = False, cpu: bool = False):
+    console.log("run fail injection")
+    console.log("target:", target, "memory:", memory, "cpu:", cpu)
+    client_cnt = 25
+    thread_cnt = 4
+    start_cluster()
+    fn = "non-fail"
+    if memory:
+        fn = "memory"
+    if cpu:
+        fn = "cpu"
+    bm_process = benchmark(client_cnt, thread_cnt,
+              f"/output/fail-injection/{target}-{fn}.txt", 1, bg=True)
+    sleep(1)
+    stress_process = None
+    if memory:
+        # sz = 200000000
+        # stress_process = sh.docker_compose(
+        #     "--compatibility", "exec", "-T", target, "stress", "--vm", "2", "--vm-bytes", sz, _bg=True, _bg_exc=False)
+        m = 1024*1024*100
+        console.log("limit memory", target, m)
+        target = f"redisraft-reliability-evaluation_{target}_1"
         try:
-            latency_process.terminate()
+            sh.docker("update", "--memory", m, "--memory-swap", 10*m, target)
         except Exception:
             pass
-        bm_process.terminate()
+    if cpu:
+        console.log("limit cpu", target, 1)
+        stress_process = sh.docker_compose(
+            "--compatibility", "exec", "-T", target, "stress", "--cpu", "1", _bg=True, _bg_exc=False)
+    console.log("wait benchmark")
+    bm_process.wait()
+    console.log("wait benchmark done")
 
-        sleep(5)
 
-console.log("end benchmark")
+for target, port in cluster:
+    run_fail_injection(target)
+    # run_fail_injection(target, memory=True)
+    # run_fail_injection(target, cpu=True)
