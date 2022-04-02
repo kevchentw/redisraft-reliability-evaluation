f95ca1bce9621d6dcdf1a233c0923095ac829623
Kuan-Yin Chen <git@kuanyin.dev>
2022-04-02 21:03:59 +0000

    updat

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from typing import List
 import sh
 from rich.console import Console
 from time import sleep
 from os import mkdir
 from datetime import datetime
-import json
+from time import time
+from tqdm import tqdm
 
 console = Console()
 
@@ -17,6 +19,13 @@ cluster = [
     ("raft2", "5002"),
     ("raft3", "5003")
 ]
+CRASH_CLIENT_CNT = 20
+CRSAH_THRED_CNT = 4
+
+def get_throughput(host, port):
+    stats_process = sh.docker_compose(
+        "exec",  "-T", "client", "redis-cli", "-p", port, "-h", host, "info", "stats")
+    return stats_process.stdout.decode().split("\r\n")[3].split(":")[1]
 
 
 def start_cluster():
@@ -37,30 +46,30 @@ def benchmark(client_cnt, thread_cnt, fn, round=1, leader=("raft1", 5001,), bg=F
     return sh.docker_compose("exec", "-T", "benchmark", "memtier_benchmark", "-p", leader[1], "-s", leader[0],
                              "-c", str(client_cnt), "-t", str(
                                  thread_cnt), "--out-file", fn,
-                             "--test-time", 10, "--data-offset", "1048575", "--data-size", "1024", "-x", round, _bg=bg, _bg_exc=False)
-
-
+                             "--data-offset", 1048576, "-x", round, 
+                             "--random-data", "--key-minimum", "200", "--key-maximum", "400",
+                             "--key-pattern", "S:S",
+                             _bg=bg, _bg_exc=False)
 def run_baseline():
     timestamp = datetime.utcnow()
     console.log("start benchmark")
-
     folder = f"./output/{timestamp}"
     mkdir(folder)
     start_cluster()
-    for client_cnt in range(1, 70, 2):
+    for client_cnt in tqdm(range(1, 40, 2)):
         console.log(client_cnt)
         for thread_cnt in [4]:
             benchmark(client_cnt, thread_cnt,
-                      f"{folder}/baseline-{client_cnt}-{thread_cnt}.txt")
+                      f"{folder}/baseline-{client_cnt}-{thread_cnt}.txt", 3)
 
         console.log("end benchmark")
 
 
-def run_fail_injection(target: str, memory: bool = False, cpu: bool = False):
+def run_fail_injection(targets: List[str], memory: bool = False, cpu: bool = False, level: int = 1):
+    client_cnt = CRASH_CLIENT_CNT
+    thread_cnt = CRSAH_THRED_CNT
     console.log("run fail injection")
-    console.log("target:", target, "memory:", memory, "cpu:", cpu)
-    client_cnt = 25
-    thread_cnt = 4
+    console.log("target:", targets, "memory:", memory, "cpu:", cpu)
     start_cluster()
     fn = "non-fail"
     if memory:
@@ -68,24 +77,33 @@ def run_fail_injection(target: str, memory: bool = False, cpu: bool = False):
     if cpu:
         fn = "cpu"
     bm_process = benchmark(client_cnt, thread_cnt,
-              f"/output/fail-injection/{target}-{fn}.txt", 1, bg=True)
-    sleep(1)
-    stress_process = None
-    if memory:
-        # sz = 200000000
-        # stress_process = sh.docker_compose(
-        #     "--compatibility", "exec", "-T", target, "stress", "--vm", "2", "--vm-bytes", sz, _bg=True, _bg_exc=False)
-        m = 1024*1024*100
-        console.log("limit memory", target, m)
-        target = f"redisraft-reliability-evaluation_{target}_1"
-        try:
-            sh.docker("update", "--memory", m, "--memory-swap", 10*m, target)
-        except Exception:
-            pass
-    if cpu:
-        console.log("limit cpu", target, 1)
-        stress_process = sh.docker_compose(
-            "--compatibility", "exec", "-T", target, "stress", "--cpu", "1", _bg=True, _bg_exc=False)
+              f"/output/fail-injection/{targets}-{fn}-{level}.txt", 3, bg=True)
+    sleep(0.5)
+    for target in targets:
+        if memory:
+            # sz = 200000000
+            # stress_process = sh.docker_compose(
+            #     "--compatibility", "exec", "-T", target, "stress", "--vm", "2", "--vm-bytes", sz, _bg=True, _bg_exc=False)
+            if level == 1:
+                m = 1024*1024*150
+            elif level == 2:
+                m = 1024*1024*100
+            elif level ==3:
+                m = 1024*1024*75
+            elif level == 4:
+                m = 1024*1024*50
+            elif level == 5:
+                m = 1024*1024*25
+            console.log("limit memory", target, m)
+            target = f"redisraft-reliability-evaluation_{target}_1"
+            try:
+                sh.docker("update", "--memory", m, "--memory-swap", 10*m, target)
+            except Exception:
+                pass
+        if cpu:
+            console.log("limit cpu", target, level)
+            stress_process = sh.docker_compose(
+                "--compatibility", "exec", "-T", target, "stress", "--cpu", level, _bg=True, _bg_exc=False)
     console.log("wait benchmark")
     bm_process.wait()
     console.log("wait benchmark done")
@@ -103,32 +121,34 @@ def get_leader():
     return ""
         
 
+
 def run_crash_leader():
+    client_cnt = CRASH_CLIENT_CNT
+    thread_cnt = CRSAH_THRED_CNT
     target = "raft1"
-    client_cnt = 25
-    thread_cnt = 4
     start_cluster()
     bm_process = benchmark(client_cnt, thread_cnt,
-              f"/output/fail-injection/{target}-crash-leader.txt", 1, bg=True)
-    sleep(1)
+              f"/output/fail-injection/{target}-crash-leader-before.txt", 3, bg=True)
+    sleep(0.5)
     sh.docker("stop", f"redisraft-reliability-evaluation_{target}_1")
-    sleep(5)
-    leader = get_leader()
-    console.log("new leader", leader)
+    leader = None
+    while not leader:
+        leader = get_leader()
+        console.log("new leader", leader)
     bm_process = benchmark(client_cnt, thread_cnt,
-            f"/output/fail-injection/{target}-crash-leader.txt", 1, leader, bg=True)
+            f"/output/fail-injection/{target}-crash-leader-after.txt", 3, leader, bg=True)
     console.log("wait benchmark")
     bm_process.wait()
     console.log("wait benchmark done")
 
 
 def run_crash_follower(target):
-    client_cnt = 25
-    thread_cnt = 4
+    client_cnt = CRASH_CLIENT_CNT
+    thread_cnt = CRSAH_THRED_CNT
     start_cluster()
     bm_process = benchmark(client_cnt, thread_cnt,
-              f"/output/fail-injection/{target}-crash-follower.txt", 1, bg=True)
-    sleep(1)
+              f"/output/fail-injection/{target}-crash-follower.txt", 3, bg=True)
+    sleep(0.5)
     sh.docker("stop", f"redisraft-reliability-evaluation_{target}_1")
     console.log("wait benchmark")
     bm_process.wait()
@@ -139,6 +159,13 @@ def run_crash_follower(target):
 #     run_fail_injection(target, memory=True)
 #     run_fail_injection(target, cpu=True)
 
-run_crash_leader()
-run_crash_follower("raft2")
-run_crash_follower("raft3")
+# run_crash_leader()
+# run_crash_follower("raft2")
+# run_crash_follower("raft3")
+
+
+# for level in [4, 5]:
+run_fail_injection([cluster[2][0], cluster[1][0]], memory=True, level=1)
+run_fail_injection([cluster[2][0], cluster[1][0]], cpu=True, level=1)
+
+# run_baseline()
